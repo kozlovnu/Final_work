12022eb1c65c370bed085a6444000c8f7e3643fa
Nikita Kozlov <kozlov.nu@gmail.com>
2022-07-28 23:11:01 +0300

    Add method ArraySizeCount

--- Program.cs
+++ Program.cs
@@ -2,4 +2,13 @@
 int newArraySize = 0;
 string[] resultArray = new string[newArraySize];
 
+int ArraySizeCount(string[] array, int newArraySize)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) 
+            newArraySize++;
+    }
+    return newArraySize;
+}
 
